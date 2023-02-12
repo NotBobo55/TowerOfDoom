ab84e7e50400df534ee34980b45c09fa3cd68017
NotBobo55 <notbobo55@gmail.com>
2023-02-12 12:37:34 +0100

    TowerOfDoom

---
+++ Assets/Scripts/DoorsScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity;
+using System;
+
+
+public class DoorsScript : MonoBehaviour
+{
+    [SerializeField] private Transform[] Destinations;
+    public Transform GetDestination()
+    {
+        int Random = UnityEngine.Random.Range(0, 12);
+        if (Random >= 8)
+        {
+            Score.instance.AddPoint();
+            return Destinations[8];
+        }
+        return Destinations[Random];
+    }
+
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static Score instance;
+    public Text ScoreText;
+    int ScoreInt = 0;
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        ScoreText.text = ScoreInt.ToString() + " POINTS";
+    }
+    public void AddPoint()
+    {
+        ScoreInt += 1;
+        ScoreText.text = ScoreInt.ToString() + " POINTS";
+    }
+}
